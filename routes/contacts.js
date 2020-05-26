2b7413efc6bf4d397a57b72377506605d194ed0e
SaiChandra2896 <saichandra2896@gmail.com>
2020-05-26 19:45:38 +0530

    added add new contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//get logged in user pvt route
-router.get('/', (req, res) => {
-    res.send('get logged in user');
+const auth = require('../middleware/auth');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
+//get all contacts of a user
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+        res.json(contacts);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server Error');
+    }
 });
 
-//Authenticate user and get token
-router.post('/', (req, res) => {
-    res.send('login user');
+//add new contact
+router.post('/', [auth, [
+    check('name', 'name is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name, email, phone, type, user: req.user.id
+        });
+
+        const contact = await newContact.save();
+        res.json(contact);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
