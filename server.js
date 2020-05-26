b844978891185dca67f3f0fbe0618ece84ad5428
SaiChandra2896 <saichandra2896@gmail.com>
2020-05-26 16:01:22 +0530

    add mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+//connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('Welcome to the Application'));
 
