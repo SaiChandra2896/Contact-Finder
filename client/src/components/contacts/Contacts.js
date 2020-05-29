b85166556b78672859b8159e76d97e4f899daad0
SaiChandra2896 <saichandra2896@gmail.com>
2020-05-29 15:13:24 +0530

    add contacs component to display contacts

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,14 @@
+import React, { Fragment, useContext } from 'react';
+import ContactContext from '../../context/contact/ContactContext';
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext);
+    const { contacts } = contactContext;
+    return (
+        <Fragment>
+            {contacts.map(contact => <h3>{contact.name}</h3>)}
+        </Fragment>
+    )
+}
+
+export default Contacts;
