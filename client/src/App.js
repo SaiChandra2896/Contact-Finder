d0ddffabd715bceb32e8173af992acf673524ffc
SaiChandra2896 <saichandra2896@gmail.com>
2020-06-05 13:17:40 +0530

    get started with auth state and context

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,26 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
 
-        </Fragment>
-      </Router>
-    </ContactState>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -4,14 +4,18 @@ import ContactItem from './ContactItem';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
+
     const { contacts, filtered } = contactContext;
+
     if (contacts.length === 0) {
         return <h4>Please Add Contacts</h4>
     }
     return (
         <Fragment>
+
             {filtered !== null ?
-                filtered.map(contact => <ContactItem key={contact.id} contact={contact} />) :
+                filtered.map(contact => (
+                    <ContactItem key={contact.id} contact={contact} />)) :
                 contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)}
         </Fragment>
     )
